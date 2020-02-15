937cca78929be2278e722336edb1c59b282f2ab2
Daniele Rubetti <d.rubetti@palmabit.com>
2020-02-15 12:49:43 +0100

    Rewrite test

---
+++ src/version/tests/version.test.ts
@@ -0,0 +1,12 @@
+import '../../utils/loadenv';
+import { version } from '../../../package.json';
+import request from 'supertest';
+import app from '../../app';
+
+describe('Version API', () => {
+  test('Should return version', async () => {
+    const { status, text } = await request(app).get('/');
+    expect(status).toBe(200);
+    expect(text).toBe(version);
+  });
+});
