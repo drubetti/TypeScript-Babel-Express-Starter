d8b70fdde51a19642ad17f925bd6c7a2545138be
Daniele Rubetti <19772982+drubetti@users.noreply.github.com>
2023-03-27 21:50:37 +0200

    Add ESLint

--- src/utils/errorsHandler.ts
+++ src/utils/errorsHandler.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 
 const errorsHandler = (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   err: any,
   req: Request,
   res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   _next: NextFunction
 ) => {
   const { message = '', statusCode = 500 } = err || {};
--- src/version/middlewares/version.ts
+++ src/version/middlewares/version.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { version } from '../../../package.json';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default (_req: Request, res: Response, _next: NextFunction) =>
   res.send(version);
