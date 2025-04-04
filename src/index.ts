089f8b979e90630db169ff8e259cd19d445b76e9
Daniele Rubetti <19772982+drubetti@users.noreply.github.com>
2025-04-04 21:58:53 +0200

    Update dependencies

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,10 @@ import logger from './utils/logger';
 const { SERVER_PORT = '3000' } = process.env;
 const port = parseInt(SERVER_PORT);
 
-app.listen(port, () => logger.info(`Server is running on port ${port}!`));
+const onAppListen = (error?: Error) => {
+  if (error) throw error;
+
+  logger.info(`Server is running on port ${port}!`);
+};
+
+app.listen(port, onAppListen);
